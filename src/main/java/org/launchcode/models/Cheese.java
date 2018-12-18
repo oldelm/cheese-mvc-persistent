586fa2616e38f0b2d2c7074d453c934696fe3175
oldelm <goldelm@gmail.com>
2018-12-18 06:52:18 -0600

    did all todos

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,11 +1,9 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -49,6 +47,9 @@ public class Cheese {
         this.category = category;
     }
 
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
 
 
---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,51 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class Menu {
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+    @Id
+    @GeneratedValue
+    private int id;
+    @ManyToMany
+    List<Cheese> cheeses;
+
+    public Menu() {
+    }
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public void addItem(Cheese item){
+        cheeses.add(item);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    //public void setCheeses(List<Cheese> cheeses) {
+    //   this.cheeses = cheeses;
+    //}
+}
